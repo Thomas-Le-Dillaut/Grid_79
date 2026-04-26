d358af18201b48b96be7b3b807653f217ecee931
Thomas-Le-Dillaut <tledilla@immacjp2.fr>
2026-04-26 08:53:22 +0000

    chat gpt a du mettre positions mais enft le dictionnaire s'appelait pos

--- Main.py
+++ Main.py
@@ -8,10 +8,81 @@ class Game():
      def __init__(self,mat):
           self.mat=mat
 
+<<<<<<< HEAD
 def someone_here(x,y):
      if (x,y) in pos.values():
           return False
      return True
+=======
+    def est_dans_plateau(self, x, y):
+        return 0 <= x < 5 and 0 <= y < 5
+
+    def afficher(self):
+        for ligne in self.mat:
+            print(ligne)
+        print("Positions :", pos)
+
+
+def case_libre(x, y):
+    return (x, y) not in pos.values()
+
+
+class Ouvrier:
+    def __init__(self, identifiant, x, y, jeu):
+        self.id = identifiant
+        self.x = x
+        self.y = y
+        self.jeu = jeu
+        pos[self.id] = (x, y)
+
+    def deplacer(self, x, y):
+        if not self.jeu.est_dans_plateau(x, y):
+            print("Hors plateau")
+            return
+
+        if abs(x - self.x) <= 1 and abs(y - self.y) <= 1:
+            if case_libre(x, y):
+                # règle : montée max de 1
+                if self.jeu.mat[x][y] <= self.jeu.mat[self.x][self.y] + 1:
+                    self.x = x
+                    self.y = y
+                    pos[self.id] = (x, y)
+                else:
+                    print("Trop haut")
+            else:
+                print("Case occupée")
+        else:
+            print("Déplacement invalide")
+
+    def construire(self, x, y):
+        if not self.jeu.est_dans_plateau(x, y):
+            print("Hors plateau")
+            return
+
+        if abs(x - self.x) <= 1 and abs(y - self.y) <= 1:
+            if case_libre(x, y):
+                if self.jeu.mat[x][y] < 4:
+                    self.jeu.mat[x][y] += 1
+                else:
+                    print("Dôme déjà présent")
+            else:
+                print("Impossible de construire ici")
+        else:
+            print("Construction invalide")
+            
+            
+jeu = Game(mat)
+
+ouvrier1 = Ouvrier(0, 0, 0, jeu)
+ouvrier2 = Ouvrier(1, 4, 4, jeu)
+
+jeu.afficher()
+
+ouvrier1.deplacer(1, 0)
+ouvrier1.construire(1, 1)
+
+jeu.afficher()
+>>>>>>> a93fa59 (chat gpt a du mettre positions mais enft le dictionnaire s'appelait pos)
 
 class ouvrier():
      def __init__(self,id):
